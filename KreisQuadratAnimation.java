bd23eac3efc7413b08c00c7aabaf1bafee2da39e
projo70 <139918856+projo70@users.noreply.github.com>
2024-08-15 09:44:34 +0200

    Add files via upload

--- KreisQuadratAnimation.java
+++ KreisQuadratAnimation.java
@@ -24,7 +24,7 @@ public class KreisQuadratAnimation extends Application{
 		kreis.setStroke(Color.BLUE);
 		//Linienbreite
 		kreis.setStrokeWidth(2);
-		//F�llfarbe
+		//Fuellfarbe
 		kreis.setFill(Color.BLACK);
 		//Position auf X- und Y-Achse festlegen
 		kreis.setLayoutX(180);
@@ -36,7 +36,7 @@ public class KreisQuadratAnimation extends Application{
 		kreis2.setStroke(Color.RED);
 		//Linienbreite
 		kreis2.setStrokeWidth(2);
-		//F�llfarbe
+		//Fuellfarbe
 		kreis2.setFill(Color.YELLOW);
 		//Position auf X- und Y-Achse festlegen
 		kreis2.setLayoutX(280);
@@ -48,7 +48,7 @@ public class KreisQuadratAnimation extends Application{
 		quadrat.setStroke(Color.GREEN);
 		//Linienbreite
 		quadrat.setStrokeWidth(2);
-		//F�llfarbe
+		//Fuellfarbe
 		quadrat.setFill(Color.BLACK);
 		//Position auf X- und Y-Achse festlegen
 		quadrat.setLayoutX(350);
@@ -60,65 +60,65 @@ public class KreisQuadratAnimation extends Application{
 		quadrat2.setStroke(Color.ORANGE);
 		//Linienbreite
 		quadrat2.setStrokeWidth(2);
-		//F�llfarbe
+		//Fuellfarbe
 		quadrat2.setFill(Color.GREEN);
 		//Position auf X- und Y-Achse festlegen
 		quadrat2.setLayoutX(250);
 		quadrat2.setLayoutY(90);
 		
-		//Instanz der Klasse ScaleTransition f�r den Kreis erstellen		
+		//Instanz der Klasse ScaleTransition fuer den Kreis erstellen		
 		ScaleTransition zoomKreis = new ScaleTransition(Duration.millis(2000),kreis);
-		//X- und Y-Achse mit dem Faktor 2 vergr��ern
+		//X- und Y-Achse mit dem Faktor 2 vergroessern
 		zoomKreis.setByX(2);
 		zoomKreis.setByY(2);
-		//Animation 10 Mal ausf�hren
-		//CycleCount auf 20 setzen damit es 10 Mal gr��er wird und 10 Mal zur�ck l�uft
+		//Animation 10 Mal ausfuehren
+		//CycleCount auf 20 setzen damit es 10 Mal groesser wird und 10 Mal zurueck laeuft
 		zoomKreis.setCycleCount(20);
-		//Animation l�uft automatisch r�ckw�rts
+		//Animation laeuft automatisch rueckwaerts
 		zoomKreis.setAutoReverse(true);
 		//Animation abspielen
 		zoomKreis.play();
 		
-		//Instanz der Klasse ScaleTransition f�r den Kreis erstellen		
+		//Instanz der Klasse ScaleTransition fuer den Kreis erstellen		
 		ScaleTransition zoomKreis2 = new ScaleTransition(Duration.millis(1000),kreis2);
-		//X- und Y-Achse mit dem Faktor 3 vergr��ern
+		//X- und Y-Achse mit dem Faktor 3 vergroessern
 		zoomKreis2.setByX(3);
 		zoomKreis2.setByY(3);
-		//Animation 20 Mal ausf�hren
-		//CycleCount auf 40 setzen damit es 20 Mal gr��er wird und 20 Mal zur�ck l�uft
+		//Animation 20 Mal ausfuehren
+		//CycleCount auf 40 setzen damit es 20 Mal groesser wird und 20 Mal zurueck laeuft
 		zoomKreis2.setCycleCount(40);
-		//Animation l�uft automatisch r�ckw�rts
+		//Animation laeuft automatisch rueckwaerts
 		zoomKreis2.setAutoReverse(true);
 		//Animation abspielen
 		zoomKreis2.play();
 		
-		//Instanz der Klasse ScaleTransition f�r das Quadrat erstellen
+		//Instanz der Klasse ScaleTransition fuer das Quadrat erstellen
 		ScaleTransition zoomQuadrat = new ScaleTransition(Duration.millis(2000),quadrat);
-		//X- und Y-Achse mit dem Faktor -0,5 vergr��ern / verkleinern
+		//X- und Y-Achse mit dem Faktor -0,5 vergroessern / verkleinern
 		zoomQuadrat.setByX(-0.5);
 		zoomQuadrat.setByY(-0.5);
-		//Animation 10 Mal ausgef�hren
-		//CycleCount auf 20 setzen damit es 10 Mal kleiner wird und 10 Mal zur�ck l�uft
+		//Animation 10 Mal ausgefuehren
+		//CycleCount auf 20 setzen damit es 10 Mal kleiner wird und 10 Mal zurueck laeuft
 		zoomQuadrat.setCycleCount(20);
-		//Animation l�uft automatisch r�ckw�rts
+		//Animation laeuft automatisch rueckwaerts
 		zoomQuadrat.setAutoReverse(true);
 		//Animation abspielen
 		zoomQuadrat.play();
 		
-		//Instanz der Klasse ScaleTransition f�r das Quadrat erstellen
+		//Instanz der Klasse ScaleTransition fuer das Quadrat erstellen
 		ScaleTransition zoomQuadrat2 = new ScaleTransition(Duration.millis(1000),quadrat2);
-		//X- und Y-Achse mit dem Faktor -0,8 vergr��ern / verkleinern
+		//X- und Y-Achse mit dem Faktor -0,8 vergroessern / verkleinern
 		zoomQuadrat2.setByX(-0.8);
 		zoomQuadrat2.setByY(-0.8);
-		//Animation 20 Mal ausf�hren
-		//CycleCount auf 40 setzen damit es 20 Mal gr��er wird und 20 Mal zur�ck l�uft
+		//Animation 20 Mal ausfuehren
+		//CycleCount auf 40 setzen damit es 20 Mal groesser wird und 20 Mal zurueck laeuft
 		zoomQuadrat2.setCycleCount(40);
-		//Animation l�uft automatisch r�ckw�rts
+		//Animation laeuft automatisch rueckwaerts
 		zoomQuadrat2.setAutoReverse(true);
 		//Animation abspielen
 		zoomQuadrat2.play();
 		
-		//Shapes hinzuf�gen
+		//Shapes hinzufuegen
 		rootNode.getChildren().add(kreis);
 		rootNode.getChildren().add(quadrat);
 		rootNode.getChildren().add(kreis2);
